34d87f467115f943ed4abbfb06d9d5325eb3a429
NicolasRomy <81150984+NicolasRomy@users.noreply.github.com>
2021-11-16 13:02:31 +0100

    hellothere

--- sfml-formation-1/sfml-formation-1/Pickup.h
+++ sfml-formation-1/sfml-formation-1/Pickup.h
@@ -1,14 +1,23 @@
-#pragma once
+/*#pragma once
 #include <SFML/Graphics.hpp>
 #include "headers/Animation.h"
 #include <vector>
+#include "Animation.cpp"
 
-
-
-class Pickup
+class HealthPotion
 {
-	Pickup(sf::Texture* texture, float SwitchTime, int column);
+public:
+	HealthPotion(sf::Texture* texture, float SwitchTime, int column);
+;
+	void Update(float deltaTime);
+	void Draw(sf::RenderWindow& window);
+	
+	sf::Sprite body;	
 	Animation animation;
+	bool IsMoving = true;
+	int row = 0;
+	int column = 0;
 
 };
 
+*/
